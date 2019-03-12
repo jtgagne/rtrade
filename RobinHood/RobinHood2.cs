96690f5aa42b0568ba912010f3996e54eeef7a99
Rascoro1 <8880966+rascoro1@users.noreply.github.com>
2019-03-12 00:09:28 -0400

    Inherits RestClient

---
+++ RobinHood/RobinHood2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using RobinHood.Properties;
+
+namespace RobinHood
+{
+    public class RobinHood : RestClient
+    {
+
+        /// <summary>
+        /// Gets the OAuth token.
+        /// </summary>
+        /// <value>The OAuth token.</value>
+        private Token OAuthToken { get; set; }
+
+        /// <summary>
+        /// Used to construct the login request. 
+        /// </summary>
+        /// <value>The login.</value>
+        private LoginRequest Login { get; set; }
+
+        public RobinHood(HttpClient client) : base(client, Settings.Default.BaseWebsite)
+        {
+        }
+
+        public void Authenticate()
+        {
+            PromptCredentials();
+            OAuthToken = HttpPost<Token>(Settings.Default.LogonEndpoint, Login);
+            if (OAuthToken.MfaRequired)
+            {
+                Console.Write("MFA Code: ");
+                string mfaCode = Console.ReadLine();
+                Login.MfaCode = mfaCode;
+                OAuthToken = HttpPost<Token>(Settings.Default.LogonEndpoint, Login);
+            }
+            OAuthToken.SaveToFile();
+            Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(OAuthToken.TokenType, OAuthToken.AccessToken);
+            
+        }
+
+        public Dictionary<string, List<Position>> GetPositions()
+        {
+            return HttpGet<Dictionary<string, List<Position>>>(Settings.Default.PostionsEndpoint);
+        }
+
+        public StockInfo GetStockInfo(string symbol)
+        {
+            string url = string.Format("{0}{1}/", Settings.Default.FundamentalsEndpoint, symbol);
+            return HttpGet<StockInfo>(url);
+        }
+
+        private void PromptCredentials()
+        {
+            this.Login = new LoginRequest();
+            Console.WriteLine("Enter user name or email:");
+            Console.Write(">>>");
+            Login.User = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            Console.Write(">>>");
+            Login.Password  = Console.ReadLine();
+        }
+    }
+}
